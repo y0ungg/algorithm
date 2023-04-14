547269ce65c3099297d4d72c0cf64a7419a4e2ec
민영 <104131962+y0ungg@users.noreply.github.com>
2023-04-15 01:26:01 +0900

    [Silver II] Title: N과 M (10), Time: 44 ms, Memory: 31256 KB -BaekjoonHub

---
+++ 백준/Silver/15664. N과 M （10）/N과 M （10）.py
@@ -0,0 +1,25 @@
+import sys
+input = sys.stdin.readline
+
+N, M = map(int, input().split())
+nums = sorted(list(map(int, input().split())))
+used = {}
+check = [0] * (N+1)
+
+def nohead(arr, depth):
+  global used
+  if (len(arr) >= M): 
+    if str(arr) not in used:
+      used[str(arr)] = 1
+      print(*arr)
+    return
+  for i in range(N):
+    if len(arr) > 0 and nums[i] < arr[-1]: continue
+    if not check[i]:
+      check[i] = 1
+      arr.append(nums[i])
+      nohead(arr, depth + 1)
+      arr.pop()
+      check[i] = 0
+
+nohead([], 1)
