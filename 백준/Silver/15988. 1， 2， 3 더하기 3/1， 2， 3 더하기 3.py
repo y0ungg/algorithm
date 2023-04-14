b2b47b9d94cb47cf349843ef89a4802e41c2bcba
민영 <104131962+y0ungg@users.noreply.github.com>
2023-04-15 01:55:07 +0900

    [Silver II] Title: 1, 2, 3 더하기 3, Time: 556 ms, Memory: 71248 KB -BaekjoonHub

---
+++ 백준/Silver/15988. 1， 2， 3 더하기 3/1， 2， 3 더하기 3.py
@@ -0,0 +1,17 @@
+import sys
+input = sys.stdin.readline
+
+l = [0, 1, 2, 4]
+def check(num):
+  if len(l) <= num:
+    while len(l) <= num:
+      count = 0
+      i = len(l)
+      count = (l[i-1] + l[i-2] + l[-3]) % 1000000009
+      l.append(count)
+  return l[num]
+        
+N = int(input())
+for i in range(N):
+  T = int(input())
+  print(check(T))
