e44b383a62ffbfcb648650c15019d1262c9cc821
민영 <104131962+y0ungg@users.noreply.github.com>
2023-06-15 15:37:08 +0900

    [Gold IV] Title: 알파벳, Time: 4948 ms, Memory: 148776 KB -BaekjoonHub

--- 백준/Gold/1987. 알파벳/알파벳.py
+++ 백준/Gold/1987. 알파벳/알파벳.py
@@ -5,12 +5,8 @@ g = [[*map(ord, input())] for _ in range(R)]
 
 alphabet = [0 for _ in range(26)]
 alphabet[g[0][0] - 65] = 1
-visited = [[0 for _ in range(C)] for _ in range(R)]
-visited[0][0] = 1
-
 delta = [(-1, 0), (0, -1), (0, 1), (1, 0)]
-res = 0
-flag = 0
+res, flag = 0, 0
 def dfs(hx, hy, count):
   global res, flag
   res = max(res, count)
@@ -20,11 +16,9 @@ def dfs(hx, hy, count):
   for dx, dy in delta:
     nx, ny = hx + dx, hy + dy
     if 0 <= nx < R and 0 <= ny < C:
-      if not visited[nx][ny] and not alphabet[g[nx][ny] - 65] and not flag:
-        visited[nx][ny] = 1
+      if not alphabet[g[nx][ny] - 65] and not flag:
         alphabet[g[nx][ny] - 65] = 1
         dfs(nx, ny, count + 1)
         alphabet[g[nx][ny] - 65] = 0
-        visited[nx][ny] = 0
 dfs(0, 0, 1)
 print(res)
